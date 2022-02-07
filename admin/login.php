dafe8dd5c08d7ec1faf6d74ea50ccf8dd6f75975
bapHyv <baptiste.hyver@gmail.com>
2022-02-07 09:57:45 +0100

    added a login page

---
+++ admin/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container mt-5 border border-info p-5 shadow-lg rounded-3">
+        <h1 class="text-center">Login</h1>
+        <form action="login.php" method="POST"></form>
+        <div>
+            <label for="utilisateur">Utilisateur:</label>
+            <input type="text" name="utilisateur" class="form-control">
+        </div>
+        <div>
+            <label for="mot_de_passe">Mot de passe:</label>
+            <input type="password" name="mot_de_passe" class="form-control">
+        </div>
+        <input type="submit" class="btn btn-success mt-5">
+        <a href="http://localhost:8888/projet_bibliotheque_clone/index.php" class="btn btn-danger mt-5">Annuler</a>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    
+</body>
+</html>
