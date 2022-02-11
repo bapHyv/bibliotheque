90052a78af5c6e18293589f96c67228206731d6a
bapHyv <baptiste.hyver@gmail.com>
2022-02-11 19:11:48 +0100

    added the pseudo and avatar of the logged in user in the top bar

--- admin/login_action.php
+++ admin/login_action.php
@@ -6,18 +6,22 @@
         $mail = htmlentities($_POST['mail']);
         $password = htmlentities($_POST['mot_de_passe']);
 
-        $sql = 'SELECT mail, mot_de_passe FROM utilisateur WHERE mail = :mail LIMIT 1';
+        $sql = 'SELECT pseudo, mail, mot_de_passe, avatar FROM utilisateur WHERE mail = :mail LIMIT 1';
         $requete = $bdd->prepare($sql);
         $data = [':mail' => $mail];
         $requete->execute($data);
         $data_user = $requete->fetch(PDO::FETCH_ASSOC);
+        $user_pseudo = $data_user['pseudo'];
         $user_mail = $data_user['mail'];
         $user_hash = $data_user['mot_de_passe'];
+        $user_avatar = $data_user['avatar'];
 
         echo '<br>';
         if (password_verify($password, $user_hash)) {
             session_start();
             $_SESSION['logged_user'] = $user_mail;
+            $_SESSION['logged_pseudo'] = $user_pseudo;
+            $_SESSION['logged_avatar'] = $user_avatar;
             header('location:index.php');
             die;
         }
--- admin/logout_action.php
+++ admin/logout_action.php
@@ -1,6 +1,8 @@
 <?php 
     session_start();
     unset($_SESSION['logged_user']);
+    unset($_SESSION['logged_pseudo']);
+    unset($_SESSION['logged_avatar']);
     session_unset();
     session_destroy();
     header('location:index.php');
